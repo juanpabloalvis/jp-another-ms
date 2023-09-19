71d46c754951794aac0fe3cd730c2c8de060adcd
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-19 10:21:59 -0500

    Initial commit

---
+++ src/main/java/com/jp/another/ms/JpAnotherMsApplication.java
@@ -0,0 +1,13 @@
+package com.jp.another.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAnotherMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAnotherMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/another/ms/JpAnotherMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.another.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAnotherMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
